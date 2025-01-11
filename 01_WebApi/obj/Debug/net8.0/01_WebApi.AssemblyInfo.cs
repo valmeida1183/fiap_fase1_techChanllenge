3171254a4c9de3ef620af3a6a9841a76d97d8d4c
Vinicius Lopes de Almeida <termobin@gmail.com>
2025-01-11 16:42:48 -0300

    Finished main

--- 01_WebApi/obj/Debug/net8.0/01_WebApi.AssemblyInfo.cs
+++ 01_WebApi/obj/Debug/net8.0/01_WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01_WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6d428add4db1fc912b3f944ef1d39e1de307c93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beb0cae139d6be375ffeb92a14fb714fdd9e240c")]
 [assembly: System.Reflection.AssemblyProductAttribute("01_WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01_WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
